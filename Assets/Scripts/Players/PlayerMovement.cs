5f54b1cba3e7db0efecfb62e50a31e8a44619608
Brendon Allen <brendon.allen09@gmail.com>
2015-05-23 18:00:56 +1200

    New ground detection system

--- Assets/Scripts/Players/PlayerMovement.cs
+++ Assets/Scripts/Players/PlayerMovement.cs
@@ -3,78 +3,66 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// Is the player grounded?
-	public int groundState = 0;
-
-	// Player Physics
+	// User configurable options
+	public float Gravity = 0.21875f;
+	public float MaxFall = 16f;
+	public float Jump = 6.5f;
 	public float Acceleration = 0.046875f;
 	public float Friction = 0.046875f;
 	public float Deceleration = 0.5f;
 	public float TopSpeed = 6f;
-	public float Gravity = 0.21875f;
-	private float JumpForce = 6.5f;
 
-	// RayCasting Objects
-	public Transform DownStart, RightDown, LeftDown;
 
-	// Private
+	// Private Objects
+	private Rect box;
+	private LayerMask lmGround;
 	public float CurrentSpeed = 0f;
 	private int PlayerDirection = 0;
 	private float FallSpeed = 0f;
 	private bool Jumping = false;
 
-	// Use this for initialization
-	void Start () {
-	
+	// Checks
+	bool grounded = false;
+	bool falling = false;
+
+	// variables for raycasting
+	private int verticalRays = 4;
+	private int margin = 2;
+
+	// Velocity
+	private Vector2 velocity;
+
+	void Start()
+	{
+		// Get layer masks by name rather than Int
+		lmGround = LayerMask.NameToLayer ("Ground");
 	}
-	
-	// Update at specific rate independant of framerate currently 50hz - See http://docs.unity3d.com/Manual/ExecutionOrder.html
-	void FixedUpdate () {
-		groundState = CheckGround();
-		Movement();
-		Raycasting();
-		UpdateAnimations();
+
+	void FixedUpdate()
+	{
+		// Grab out box collider and make rect object box for easier user
+		BoxCollider collider = GetComponent<BoxCollider>();
+		box = new Rect (collider.bounds.min.x,
+		               collider.bounds.min.y,
+		               collider.bounds.size.x,
+		               collider.bounds.size.y);
+		GroundState ();
+		DMovement ();
+		UpdateAnimations ();
+		transform.Translate (velocity);
 	}
 
 	void OnGUI()
 	{
-		GUILayout.Label( "GroundState = " + groundState + "\n0=Not Grounded, 1=Grounded, 2=Front Not Grounded, Back Is Grounded (Sonic Balancing Animation), 3=Sonic Balancing Other Direction");
+		GUILayout.Label ("Grounded: " + grounded + ", Falling: " + falling);
 		GUILayout.Label ("Location: " + transform.position.ToString());
-		GUILayout.Label ("Current Speed: " + CurrentSpeed.ToString());
-		GUILayout.Label ("Fall Speed: " + FallSpeed.ToString());
-		GUILayout.Label ("Direction: " + PlayerDirection.ToString ());
+		GUILayout.Label ("Velocity: " + velocity.ToString ());
 	}    
-
-	void Movement(){
-
-		if (groundState == 0) {
-
-			FallSpeed = FallSpeed + Gravity;
-
-			if (FallSpeed > 16f)
-			{
-				FallSpeed = 16f;
-			}
-
-			// Move Sprite with Z Axis Locked to 0
-			transform.Translate(-Vector2.up * FallSpeed, 0);
-		}
-		 // Directional Left and Right Movement
-		DMovement();
-
-		if (Input.GetKey (KeyCode.Space)) {
-			Jumping = true;
-		}
-
-		if (Jumping == true) {
-			transform.Translate(Vector2.up * JumpForce, 0);
-		}
-	}
 
 	void DMovement()
 	{
 		if (Input.GetKey (KeyCode.LeftArrow)) {
-
+			
 			if (CurrentSpeed > 0) {
 				CurrentSpeed = CurrentSpeed - Deceleration;
 			} else {
@@ -82,15 +70,15 @@ public class PlayerMovement : MonoBehaviour {
 				{
 					CurrentSpeed = CurrentSpeed - Acceleration;
 				}else{
-				CurrentSpeed = -TopSpeed;
+					CurrentSpeed = -TopSpeed;
 				}
 			}
-
+			
 			// Turn Sprite Left
 			transform.eulerAngles = new Vector2 (0, 180);
-
+			
 		} else if (Input.GetKey (KeyCode.RightArrow)) {
-
+			
 			if (CurrentSpeed < 0) {
 				CurrentSpeed = CurrentSpeed + Deceleration;
 			} else {
@@ -101,23 +89,15 @@ public class PlayerMovement : MonoBehaviour {
 					CurrentSpeed = TopSpeed;
 				}
 			}
-
+			
 			// Turn Sprite Right
 			transform.eulerAngles = new Vector2 (0, 0);
-	
+			
 		} else {
 			CurrentSpeed = CurrentSpeed - (Mathf.Min(Mathf.Abs(CurrentSpeed), Friction)*Mathf.Sign(CurrentSpeed));
 		}
-
-		transform.Translate (Vector2.right * CurrentSpeed, 0);
-
-
-	}
-
-	void Raycasting() {
-		Debug.DrawLine (DownStart.position, LeftDown.position, Color.green, 0.2f, false);
-		Debug.DrawLine (DownStart.position, RightDown.position, Color.green, 0.2f, false);
-
+		velocity = new Vector2 (Mathf.Abs(CurrentSpeed), velocity.y);		
+		
 	}
 
 	void UpdateAnimations()
@@ -127,34 +107,48 @@ public class PlayerMovement : MonoBehaviour {
 		anim.SetBool ("Jumping", Jumping);
 	}
 
-	/// <summary>
-	/// Check the Player Grounding Status
-	/// </summary>
-	/// <returns>Grounding Interger Value</returns>
-	private int CheckGround()
-	{
-		bool g1 = Physics.Linecast (DownStart.position, RightDown.position);
-		bool g2 = Physics.Linecast (DownStart.position, LeftDown.position);
 
-		// We are fully grounded both sensors are intersecting
-		if (g1 && g2) {
-			FallSpeed = 0f;
-			Jumping = false;
-			return 1;
+	void GroundState()
+	{
+		// Not grounded apply gravity
+		if (!grounded) {
+			velocity = new Vector2 (velocity.x, Mathf.Max (velocity.y - Gravity, -MaxFall));
 		}
-		// Near a ledge, this is when we need to change to the Balancing Animations
-		if (g1 == false && g2 == true) {
-			FallSpeed = 0f;
-			Jumping = false;
-			return 2;
+		// Were falling
+		if (velocity.y < 0) {
+			falling = true;
 		}
-		if (g2 == false && g1 == true) {
-			FallSpeed = 0f;
-			Jumping = false;
-			return 3;		
-		// Nothing below us, oh crap were falling.
-		} else {
-			return 0;
+		//
+		if (grounded || falling) {
+			Vector3 startPoint = new Vector3(box.xMin + margin, box.center.y, transform.position.z);
+			Vector3 endPoint = new Vector3(box.xMax - margin, box.center.y, transform.position.z);
+
+			RaycastHit hitInfo;
+
+			float distance = box.height/2+(grounded? margin: Mathf.Abs (velocity.y));
+
+			bool connected = false;
+
+			for (int i =0; i < verticalRays; i++){
+				float lerpAmount = (float)i / (float) verticalRays -1;
+				Vector3 origin = Vector3.Lerp (startPoint, endPoint, lerpAmount);
+				Ray ray = new Ray(origin, Vector3.down);
+
+				connected = Physics.Raycast(ray, out hitInfo, distance,1 << lmGround);
+
+				if (connected){
+					grounded = true;
+					falling = false;
+					transform.Translate(Vector3.down * (hitInfo.distance - box.height/2));
+					velocity = new Vector2(velocity.x, 0);
+					break;
+				}
+			}
+			if (!connected){
+				grounded = false;
+			}
 		}
+
 	}
+
 }
