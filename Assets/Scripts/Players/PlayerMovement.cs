d42735c898e5d12c323105377095049dc646b45a
Brendon Allen <brendon.allen09@gmail.com>
2015-06-06 22:47:17 +1200

    Jumping Fix, Physics Changes
    Added Variable jump heights
    Updated Coding standards to clean up code

--- Assets/Scripts/Players/PlayerMovement.cs
+++ Assets/Scripts/Players/PlayerMovement.cs
@@ -1,17 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
-	private const float FacingRight = 0f;
-	private const float FacingLeft = 180f;
+    #region Constants
+    private const float FACING_RIGHT = 0f;
+    private const float FACING_LEFT = 180f;
+    #endregion
 
-	// User configurable options
-	public float Gravity = 0.21875f;
-	public float MaxFall = 16f;
-	public float Jump = 6.5f;
-	public float Acceleration = 0.046875f;
-	public float Friction = 0.046875f;
+    #region Public Variables
+    public float GRAVITY = 0.21875f;
+    public float MAX_FALL_VELOCITY = 16f;
+    public float MAX_JUMP_FORCE = 6.5f;
+    public float MIN_JUMP_FORCE = 4f;
+    public float ACCELERATION = 0.046875f;
+    #endregion
+
+
+    #region Player States
+    //	; ===========================================================================
+	//		; off_19F6A: Obj01_States:
+	//		Obj01_Index:	offsetTable
+	//			offsetTableEntry.w Obj01_Init		;  0
+	//		offsetTableEntry.w Obj01_Control	    ;  2
+	//		offsetTableEntry.w Obj01_Hurt		    ;  4
+	//		offsetTableEntry.w Obj01_Dead		    ;  6
+	//		offsetTableEntry.w Obj01_Gone		    ;  8
+	//		offsetTableEntry.w Obj01_Respawning	    ; 10
+
+	private int PlayerState = 0;
+
+	#endregion
+
+	#region Player Modes
+
+	//  ; ===========================================================================
+	//	; secondary states under state Obj01_Control
+	//	; off_1A0BE:
+	//	Obj01_Modes:	offsetTable
+	//	offsetTableEntry.w Obj01_MdNormal_Checks	; 0 - not airborne or rolling
+	//	offsetTableEntry.w Obj01_MdAir			    ; 2 - airborne
+	//	offsetTableEntry.w Obj01_MdRoll			    ; 4 - rolling
+	//	offsetTableEntry.w Obj01_MdJump			    ; 6 - jumping
+	private int PlayerMode = 0;
+
+	#endregion
+
+    #region Private Variables
+    private bool _jump = false;
+    private bool _jumping = false;
+    private bool _jumpCancel = false;
+    #endregion
+
+
+    public float Friction = 0.046875f;
 	public float Deceleration = 0.5f;
 	public float TopSpeed = 6f;
 
@@ -21,13 +64,17 @@ public class PlayerMovement : MonoBehaviour {
 	private LayerMask lmGround;
 	public float CurrentSpeed = 0f;
 	private float FallSpeed = 0f;
-	private bool Jumping = false;
+
 	private bool StartJump = false;
+    
 	private int JumpHold = 0;		// Counts if jump is held down
+    private int JumpHold1 = 0;
 	private bool FrontEdge = false;
 	private bool BackEdge = false;
 
 	private float FEdgeDistance = 0f;
+
+
 
 	// Ground Sensor Values
 	private Vector2 SensorGroundA, SensorGroundB;
@@ -53,40 +100,59 @@ public class PlayerMovement : MonoBehaviour {
 
 	void FixedUpdate()
 	{
-		// Grab out box collider and make rect object box for easier user
-		BoxCollider collider = GetComponent<BoxCollider>();
-		box = new Rect (collider.bounds.min.x,
-		               collider.bounds.min.y,
-		               collider.bounds.size.x,
-		               collider.bounds.size.y);
-		GroundState ();
-		DMovement ();
-		if (Input.GetKey (KeyCode.A) && grounded) {
-			StartJump = true;
-			JumpHold += 1;
-		} else {
-			//JumpHold = 0;
-		}
-		if (StartJump){
 
-			Jumping = true;
-			StartJump = false;
-			velocity = new Vector2 (velocity.x, Jump);
-		}
-		UpdateAnimations ();
-		transform.eulerAngles = new Vector2(0,YRotation);
-		transform.Translate (velocity.x,velocity.y,0f);
 	}
+
+    void Update()
+    {
+        // Grab out box collider and make rect object box for easier user
+        BoxCollider collider = GetComponent<BoxCollider>();
+        box = new Rect(collider.bounds.min.x,
+                       collider.bounds.min.y,
+                       collider.bounds.size.x,
+                       collider.bounds.size.y);
+        GroundState();
+        Jumping();
+        DMovement();
+        UpdateAnimations();
+        transform.eulerAngles = new Vector2(0, YRotation);
+        transform.Translate(velocity.x, velocity.y, 0f);
+    }
 
 	void OnGUI()
 	{
-		GUILayout.Label ("Grounded: " + grounded + ", Falling: " + falling + ", Jumping: " + Jumping + ", Edge: " + FrontEdge);
+		GUILayout.Label ("Grounded: " + grounded + ", Falling: " + falling + ", Jumping: " + _jumping + ", Front Edge: " + FrontEdge + ", Back Edge: " + BackEdge);
 		GUILayout.Label ("Location: " + transform.position.ToString());
 		GUILayout.Label ("Velocity: " + velocity.ToString ());
 		GUILayout.Label ("Sensor A: " + SensorGroundA.ToString() + ", Sensor B: " + SensorGroundB.ToString ());
 		GUILayout.Label ("Jump Hold: " + JumpHold.ToString ());
 		GUILayout.Label ("Edge Detect Distance: " + FEdgeDistance.ToString());
-	}    
+	}
+
+    void Jumping()
+    {
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            _jumping = true;
+            _jump = true;
+        }
+        if (_jump)
+        {
+            velocity = new Vector2(velocity.x, MAX_JUMP_FORCE);
+            _jump = false;
+        }
+        if (Input.GetButtonUp("Jump") && !grounded)
+        {
+            _jumpCancel = true;
+        }
+        if (_jumpCancel)
+        {
+            if (velocity.y > MIN_JUMP_FORCE)
+                velocity = new Vector2(velocity.x, MIN_JUMP_FORCE);
+            _jumpCancel = false;
+        }
+    }
 
 	void DMovement()
 	{
@@ -97,14 +163,14 @@ public class PlayerMovement : MonoBehaviour {
 			} else {
 				if (CurrentSpeed > -TopSpeed)
 				{
-					CurrentSpeed = CurrentSpeed - Acceleration;
+					CurrentSpeed = CurrentSpeed - ACCELERATION;
 				}else{
 					CurrentSpeed = -TopSpeed;
 				}
 			}
 			
 			// Turn Sprite Left
-			YRotation = FacingLeft;
+			YRotation = FACING_LEFT;
 			
 		} else if (Input.GetKey (KeyCode.RightArrow)) {
 			
@@ -113,14 +179,14 @@ public class PlayerMovement : MonoBehaviour {
 			} else {
 				if (CurrentSpeed < TopSpeed)
 				{
-					CurrentSpeed = CurrentSpeed + Acceleration;
+					CurrentSpeed = CurrentSpeed + ACCELERATION;
 				}else{
 					CurrentSpeed = TopSpeed;
 				}
 			}
 			
 			// Turn Sprite Right
-			YRotation = FacingRight;
+			YRotation = FACING_RIGHT;
 			
 		} else {
 			CurrentSpeed = CurrentSpeed - (Mathf.Min(Mathf.Abs(CurrentSpeed), Friction)*Mathf.Sign(CurrentSpeed));
@@ -131,12 +197,9 @@ public class PlayerMovement : MonoBehaviour {
 
 	void UpdateAnimations()
 	{
-
-
-
 		Animator anim = GetComponent<Animator> ();
 		anim.SetFloat ("Speed", CurrentSpeed);
-		anim.SetBool ("Jumping", Jumping);
+		anim.SetBool ("Jumping", _jumping);
 	}
 
 	void GroundState()
@@ -145,7 +208,7 @@ public class PlayerMovement : MonoBehaviour {
 
 		// Not grounded apply gravity
 		if (!grounded) {
-			velocity = new Vector2 (velocity.x, Mathf.Max (velocity.y - Gravity, -MaxFall));
+			velocity = new Vector2 (velocity.x, Mathf.Max (velocity.y - GRAVITY, -MAX_FALL_VELOCITY));
 		}
 		// Were falling
 		if (velocity.y < 0) {
@@ -155,10 +218,10 @@ public class PlayerMovement : MonoBehaviour {
 
 			// Check if we are jumping, if so change the width of the Sensors
 			// Also check the roataion and swap the A and B sensors depending on what way we are facing
-			switch(Jumping)
+			switch(_jumping)
 			{
 			case true:
-				if (YRotation == FacingRight)
+				if (YRotation == FACING_RIGHT)
 				{
 					vGaStart = new Vector3(box.center.x + 7,box.center.y,transform.position.z);
 					vGbStart = new Vector3(box.center.x - 7,box.center.y,transform.position.z);
@@ -169,7 +232,7 @@ public class PlayerMovement : MonoBehaviour {
 				}
 				break;
 			default:
-				if (YRotation == FacingRight)
+				if (YRotation == FACING_RIGHT)
 				{
 					vGaStart = new Vector3(box.center.x + 9,box.center.y,transform.position.z);
 					vGbStart = new Vector3(box.center.x - 9,box.center.y,transform.position.z);
@@ -203,10 +266,10 @@ public class PlayerMovement : MonoBehaviour {
 			// If anything collides were on the floor
 			if (bGaConnected || bGbConnected)
 			{
+                _jumping = false;
 				grounded = true;
 				falling = false;
-				Jumping = false;
-
+			
 				// Store out the sensor values
 				if (hGa.collider != null) {SensorGroundA = hGa.point;}
 				if (hGb.collider != null) {SensorGroundB = hGb.point;}
@@ -221,7 +284,6 @@ public class PlayerMovement : MonoBehaviour {
 					transform.Translate(Vector3.down * (hGb.distance - box.height/2)); // Places the transform on the ground
 				}
 
-
 				velocity = new Vector2(velocity.x, 0);
 			}
 
@@ -232,7 +294,7 @@ public class PlayerMovement : MonoBehaviour {
 			}
 
 			// Edge detection for the Balancing Animations
-			if (!bGaConnected && bGbConnected && !Jumping)
+			if (!bGaConnected && bGbConnected && !_jumping)
 			{
 				FrontEdge = true;
 				FEdgeDistance = ((SensorGroundA.x - hGb.point.x) / 2);
