5b62afb266b964db10e9f3f4a2c0b08920446ea4
Brendon Allen <brendon.allen09@gmail.com>
2015-06-07 16:18:50 +1200

    ADDED: Balancing Animation
    Added the first balancing animation
    Also some code cleanups

--- Assets/Scripts/Players/PlayerMovement.cs
+++ Assets/Scripts/Players/PlayerMovement.cs
@@ -15,14 +15,17 @@ public class PlayerMovement : MonoBehaviour
     public float MAX_JUMP_FORCE = 6.5f;
     public float MIN_JUMP_FORCE = 4f;
     public float ACCELERATION = 0.046875f;
+    public float FRICTION = 0.046875f;
+    public float DECELERATION = 0.5f;
+    public float TOP_SPEED = 6f;
     #endregion
 
 
     #region Player States
-    //	; ===========================================================================
+    //	    ; ===========================================================================
 	//		; off_19F6A: Obj01_States:
 	//		Obj01_Index:	offsetTable
-	//			offsetTableEntry.w Obj01_Init		;  0
+	//		offsetTableEntry.w Obj01_Init		    ;  0
 	//		offsetTableEntry.w Obj01_Control	    ;  2
 	//		offsetTableEntry.w Obj01_Hurt		    ;  4
 	//		offsetTableEntry.w Obj01_Dead		    ;  6
@@ -51,12 +54,12 @@ public class PlayerMovement : MonoBehaviour
     private bool _jump = false;
     private bool _jumping = false;
     private bool _jumpCancel = false;
+    private bool _moveLeft = false;
+    private bool _moveRight = false;
+    private bool _edgeFront = false;
+    private bool _edgeBack = false;
+    private float _edgeDistance = 0f;
     #endregion
-
-
-    public float Friction = 0.046875f;
-	public float Deceleration = 0.5f;
-	public float TopSpeed = 6f;
 
 
 	// Private Objects
@@ -67,12 +70,9 @@ public class PlayerMovement : MonoBehaviour
 
 	private bool StartJump = false;
     
-	private int JumpHold = 0;		// Counts if jump is held down
-    private int JumpHold1 = 0;
-	private bool FrontEdge = false;
-	private bool BackEdge = false;
 
-	private float FEdgeDistance = 0f;
+
+	
 
 
 
@@ -110,9 +110,8 @@ public class PlayerMovement : MonoBehaviour
                        collider.bounds.size.x,
                        collider.bounds.size.y);
         GroundState();
-        Jumping();
+        CheckJump();
         DMovement();
-        UpdateAnimations();
         transform.eulerAngles = new Vector2(0, YRotation);
         transform.Translate(velocity.x, velocity.y, 0f);
 	}
@@ -123,16 +122,17 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         KeyUpdates();
+        UpdateAnimations();
     }
 
 	void OnGUI()
 	{
-		GUILayout.Label ("Grounded: " + grounded + ", Falling: " + falling + ", Jumping: " + _jumping + ", Front Edge: " + FrontEdge + ", Back Edge: " + BackEdge);
+        //GUILayout.Label ("Grounded: " + grounded + ", Falling: " + falling + ", Jumping: " + _jumping + ", Front Edge: " + FrontEdge + ", Back Edge: " + BackEdge);
 		GUILayout.Label ("Location: " + transform.position.ToString());
 		GUILayout.Label ("Velocity: " + velocity.ToString ());
+        GUILayout.Label("Current Speed: " + CurrentSpeed.ToString());
 		GUILayout.Label ("Sensor A: " + SensorGroundA.ToString() + ", Sensor B: " + SensorGroundB.ToString ());
-		GUILayout.Label ("Jump Hold: " + JumpHold.ToString ());
-		GUILayout.Label ("Edge Detect Distance: " + FEdgeDistance.ToString());
+		GUILayout.Label ("Edge Detect Distance: " + _edgeDistance.ToString());
 	}
 
     /// <summary>
@@ -140,19 +140,127 @@ public class PlayerMovement : MonoBehaviour
     /// </summary>
     void KeyUpdates()
     {
-        if (Input.GetButtonDown("Jump") && grounded)
-        {
-            _jump = true;
-        }
-        if (Input.GetButtonUp("Jump") && !grounded)
-        {
-            _jumpCancel = true;
-        }
+        // Handle Jump
+        if (Input.GetButtonDown("Jump") && grounded) { _jump = true; }
+        if (Input.GetButtonUp("Jump") && !grounded) { _jumpCancel = true; }
+
+        // Handle D Movement
+        if (Input.GetKey(KeyCode.LeftArrow))  { _moveLeft = true; } else { _moveLeft = false; }
+        if (Input.GetKey(KeyCode.RightArrow)) { _moveRight = true; } else { _moveRight = false; }
     }
 
-    void Jumping()
+    /// <summary>
+    /// Check For Jumps
+    /// </summary>
+    void CheckJump()
     {
+        /// When you release the jump button in the air after jumping, the computer checks to see if Sonic is moving upward (i.e. Y speed is negative). If he is, then it checks to see if Y speed is less than -4 (e.g. -5 is "less" than -4). If it is, then Y speed is set to -4. In this way, you can cut your jump short at any time, just by releasing the jump button. If you release the button in the very next step after jumping, Sonic makes the shortest possible jump.
+        
+        #region Jump Assembly
+        //        ; ---------------------------------------------------------------------------
+        //; Subroutine allowing Sonic to jump
+        //; ---------------------------------------------------------------------------
 
+        //; ||||||||||||||| S U B R O U T I N E |||||||||||||||||||||||||||||||||||||||
+
+        //; loc_1AA38:
+        //Sonic_Jump:
+        //    move.b	(Ctrl_1_Press_Logical).w,d0
+        //    andi.b	#button_B_mask|button_C_mask|button_A_mask,d0 ; is A, B or C pressed?
+        //    beq.w	return_1AAE6	; if not, return
+        //    moveq	#0,d0
+        //    move.b	angle(a0),d0
+        //    addi.b	#$80,d0
+        //    bsr.w	CalcRoomOverHead
+        //    cmpi.w	#6,d1			; does Sonic have enough room to jump?
+        //    blt.w	return_1AAE6		; if not, branch
+        //    move.w	#$680,d2
+        //    tst.b	(Super_Sonic_flag).w
+        //    beq.s	+
+        //    move.w	#$800,d2	; set higher jump speed if super
+        //+
+        //    btst	#6,status(a0)	; Test if underwater
+        //    beq.s	+
+        //    move.w	#$380,d2	; set lower jump speed if under
+        //+
+        //    moveq	#0,d0
+        //    move.b	angle(a0),d0
+        //    subi.b	#$40,d0
+        //    jsr	(CalcSine).l
+        //    muls.w	d2,d1
+        //    asr.l	#8,d1
+        //    add.w	d1,x_vel(a0)	; make Sonic jump (in X... this adds nothing on level ground)
+        //    muls.w	d2,d0
+        //    asr.l	#8,d0
+        //    add.w	d0,y_vel(a0)	; make Sonic jump (in Y)
+        //    bset	#1,status(a0)
+        //    bclr	#5,status(a0)
+        //    addq.l	#4,sp
+        //    move.b	#1,jumping(a0)
+        //    clr.b	stick_to_convex(a0)
+        //    move.w	#SndID_Jump,d0
+        //    jsr	(PlaySound).l	; play jumping sound
+        //    move.b	#$13,y_radius(a0)
+        //    move.b	#9,x_radius(a0)
+        //    btst	#2,status(a0)
+        //    bne.s	Sonic_RollJump
+        //    move.b	#$E,y_radius(a0)
+        //    move.b	#7,x_radius(a0)
+        //    move.b	#AniIDSonAni_Roll,anim(a0)	; use "jumping" animation
+        //    bset	#2,status(a0)
+        //    addq.w	#5,y_pos(a0)
+
+        //return_1AAE6:
+        //    rts
+        //; ---------------------------------------------------------------------------
+        //; loc_1AAE8:
+        //Sonic_RollJump:
+        //    bset	#4,status(a0)	; set the rolling+jumping flag
+        //    rts
+        //; End of function Sonic_Jump
+        
+
+        //; ---------------------------------------------------------------------------
+        //; Subroutine letting Sonic control the height of the jump
+        //; when the jump button is released
+        //; ---------------------------------------------------------------------------
+
+        //; ||||||||||||||| S U B R O U T I N E |||||||||||||||||||||||||||||||||||||||
+
+        //        ; ===========================================================================
+        //; loc_1AAF0:
+        //Sonic_JumpHeight:
+        //    tst.b	jumping(a0)	; is Sonic jumping?
+        //    beq.s	Sonic_UpVelCap	; if not, branch
+
+        //    move.w	#-$400,d1
+        //    btst	#6,status(a0)	; is Sonic underwater?
+        //    beq.s	+		; if not, branch
+        //    move.w	#-$200,d1
+        //+
+        //    cmp.w	y_vel(a0),d1	; is Sonic going up faster than d1?
+        //    ble.s	+		; if not, branch
+        //    move.b	(Ctrl_1_Held_Logical).w,d0
+        //    andi.b	#button_B_mask|button_C_mask|button_A_mask,d0 ; is a jump button pressed?
+        //    bne.s	+		; if yes, branch
+        //    move.w	d1,y_vel(a0)	; immediately reduce Sonic's upward speed to d1
+        //+
+        //    tst.b	y_vel(a0)		; is Sonic exactly at the height of his jump?
+        //    beq.s	Sonic_CheckGoSuper	; if yes, test for turning into Super Sonic
+        //    rts
+        //; ---------------------------------------------------------------------------
+        //; loc_1AB22:
+        //Sonic_UpVelCap:
+        //    tst.b	spindash_flag(a0)	; is Sonic charging a spindash or in a rolling-only area?
+        //    bne.s	return_1AB36		; if yes, return
+        //    cmpi.w	#-$FC0,y_vel(a0)	; is Sonic moving up really fast?
+        //    bge.s	return_1AB36		; if not, return
+        //    move.w	#-$FC0,y_vel(a0)	; cap upward speed
+
+        //return_1AB36:
+        //    rts
+        //; End of subroutine Sonic_JumpHeight
+        #endregion
 
         if (_jump)
         {
@@ -171,32 +279,32 @@ public class PlayerMovement : MonoBehaviour
 
 	void DMovement()
 	{
-		if (Input.GetKey (KeyCode.LeftArrow)) {
+		if (_moveLeft) {
 			
-			if (CurrentSpeed > 0) {
-				CurrentSpeed = CurrentSpeed - Deceleration;
+			if (CurrentSpeed > 0f) {
+				CurrentSpeed = CurrentSpeed - DECELERATION;
 			} else {
-				if (CurrentSpeed > -TopSpeed)
+				if (CurrentSpeed > -TOP_SPEED)
 				{
 					CurrentSpeed = CurrentSpeed - ACCELERATION;
 				}else{
-					CurrentSpeed = -TopSpeed;
+					CurrentSpeed = -TOP_SPEED;
 				}
 			}
 			
 			// Turn Sprite Left
 			YRotation = FACING_LEFT;
 			
-		} else if (Input.GetKey (KeyCode.RightArrow)) {
+		} else if (_moveRight) {
 			
-			if (CurrentSpeed < 0) {
-				CurrentSpeed = CurrentSpeed + Deceleration;
+			if (CurrentSpeed < 0f) {
+                CurrentSpeed = CurrentSpeed + DECELERATION;
 			} else {
-				if (CurrentSpeed < TopSpeed)
+				if (CurrentSpeed < TOP_SPEED)
 				{
 					CurrentSpeed = CurrentSpeed + ACCELERATION;
 				}else{
-					CurrentSpeed = TopSpeed;
+					CurrentSpeed = TOP_SPEED;
 				}
 			}
 			
@@ -204,8 +312,9 @@ public class PlayerMovement : MonoBehaviour
 			YRotation = FACING_RIGHT;
 			
 		} else {
-			CurrentSpeed = CurrentSpeed - (Mathf.Min(Mathf.Abs(CurrentSpeed), Friction)*Mathf.Sign(CurrentSpeed));
+			CurrentSpeed = CurrentSpeed - (Mathf.Min(Mathf.Abs(CurrentSpeed), FRICTION)*Mathf.Sign(CurrentSpeed));
 		}
+
 		velocity = new Vector2 (Mathf.Abs(CurrentSpeed), velocity.y);		
 		
 	}
@@ -215,6 +324,8 @@ public class PlayerMovement : MonoBehaviour
 		Animator anim = GetComponent<Animator> ();
 		anim.SetFloat ("Speed", CurrentSpeed);
 		anim.SetBool ("Jumping", _jumping);
+        anim.SetFloat("EdgeDistance", _edgeDistance);
+        anim.SetBool("FrontEdge", _edgeFront);
 	}
 
 	void GroundState()
@@ -305,18 +416,18 @@ public class PlayerMovement : MonoBehaviour
 			// Uh Oh not on the ground here, were going to fall :O
 			if (!bGaConnected && !bGbConnected){
 				grounded = false;
-				FrontEdge = false;
+				_edgeFront = false;
 			}
 
 			// Edge detection for the Balancing Animations
 			if (!bGaConnected && bGbConnected && !_jumping)
 			{
-				FrontEdge = true;
-				FEdgeDistance = ((SensorGroundA.x - hGb.point.x) / 2);
+				_edgeFront = true;
+				_edgeDistance = ((SensorGroundA.x - hGb.point.x) / 2);
 			}
 			else if (bGaConnected && bGbConnected)
 			{
-				FrontEdge = false;
+				_edgeFront = false;
 			}
 		}
 	}
