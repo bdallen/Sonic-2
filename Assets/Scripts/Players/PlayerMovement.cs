a44603a07e970862a7932a8182beb74c0f597810
Brendon Allen <brendon.allen09@gmail.com>
2015-06-08 22:04:21 +1200

    BUGFIX: Partial fix on the Bouncing issue on Phy
    Commented in the TODO: Part of the ground raycasts were causing the
    issue of collision detection and bouncing when hitting the ground.

--- Assets/Scripts/Players/PlayerMovement.cs
+++ Assets/Scripts/Players/PlayerMovement.cs
@@ -55,9 +55,10 @@ public class PlayerMovement : MonoBehaviour
     private bool _jumpCancel = false;
     private bool _moveLeft = false;
     private bool _moveRight = false;
-    private bool _edgeFront = false;
-    private bool _edgeBack = false;
+    private bool _edgeFound = false;
     private float _edgeDistance = 0f;
+    private float _hgaDistance = 0f;
+    private float _hgbDistance = 0f;
     #endregion
 
 
@@ -65,9 +66,6 @@ public class PlayerMovement : MonoBehaviour
 	private Rect box;
 	private LayerMask lmGround;
 	public float CurrentSpeed = 0f;
-	private float FallSpeed = 0f;
-
-	private bool StartJump = false;
     
 	// Ground Sensor Values
 	private Vector2 SensorGroundA, SensorGroundB;
@@ -78,7 +76,6 @@ public class PlayerMovement : MonoBehaviour
 	bool falling = false;
 
 	// variables for raycasting
-	private int verticalRays = 2;
 	private int margin = 1;
 
 	// Velocity and Rotation
@@ -89,6 +86,7 @@ public class PlayerMovement : MonoBehaviour
 	{
 		// Get layer masks by name rather than Int
 		lmGround = LayerMask.NameToLayer ("Ground");
+        Application.targetFrameRate = 50;
 	}
 
     /// <summary>
@@ -102,13 +100,15 @@ public class PlayerMovement : MonoBehaviour
                        collider.bounds.min.y,
                        collider.bounds.size.x,
                        collider.bounds.size.y);
+
+        Collision();
         GroundState();
+        ApplyGravity();
         CheckJump();
         DMovement();
-        ApplyGravity();
-
         transform.eulerAngles = new Vector2(0, YRotation);
         transform.Translate(velocity.x, velocity.y, 0f);
+        
 	}
 
     /// <summary>
@@ -124,10 +124,11 @@ public class PlayerMovement : MonoBehaviour
         {
             Time.timeScale = 1f;
         }
+
+
         KeyUpdates();
         UpdateAnimations();
-
-    }
+  }
 
 	void OnGUI()
 	{
@@ -159,113 +160,7 @@ public class PlayerMovement : MonoBehaviour
     void CheckJump()
     {
         /// When you release the jump button in the air after jumping, the computer checks to see if Sonic is moving upward (i.e. Y speed is negative). If he is, then it checks to see if Y speed is less than -4 (e.g. -5 is "less" than -4). If it is, then Y speed is set to -4. In this way, you can cut your jump short at any time, just by releasing the jump button. If you release the button in the very next step after jumping, Sonic makes the shortest possible jump.
-        
-        #region Jump Assembly
-        //        ; ---------------------------------------------------------------------------
-        //; Subroutine allowing Sonic to jump
-        //; ---------------------------------------------------------------------------
-
-        //; ||||||||||||||| S U B R O U T I N E |||||||||||||||||||||||||||||||||||||||
-
-        //; loc_1AA38:
-        //Sonic_Jump:
-        //    move.b	(Ctrl_1_Press_Logical).w,d0
-        //    andi.b	#button_B_mask|button_C_mask|button_A_mask,d0 ; is A, B or C pressed?
-        //    beq.w	return_1AAE6	; if not, return
-        //    moveq	#0,d0
-        //    move.b	angle(a0),d0
-        //    addi.b	#$80,d0
-        //    bsr.w	CalcRoomOverHead
-        //    cmpi.w	#6,d1			; does Sonic have enough room to jump?
-        //    blt.w	return_1AAE6		; if not, branch
-        //    move.w	#$680,d2
-        //    tst.b	(Super_Sonic_flag).w
-        //    beq.s	+
-        //    move.w	#$800,d2	; set higher jump speed if super
-        //+
-        //    btst	#6,status(a0)	; Test if underwater
-        //    beq.s	+
-        //    move.w	#$380,d2	; set lower jump speed if under
-        //+
-        //    moveq	#0,d0
-        //    move.b	angle(a0),d0
-        //    subi.b	#$40,d0
-        //    jsr	(CalcSine).l
-        //    muls.w	d2,d1
-        //    asr.l	#8,d1
-        //    add.w	d1,x_vel(a0)	; make Sonic jump (in X... this adds nothing on level ground)
-        //    muls.w	d2,d0
-        //    asr.l	#8,d0
-        //    add.w	d0,y_vel(a0)	; make Sonic jump (in Y)
-        //    bset	#1,status(a0)
-        //    bclr	#5,status(a0)
-        //    addq.l	#4,sp
-        //    move.b	#1,jumping(a0)
-        //    clr.b	stick_to_convex(a0)
-        //    move.w	#SndID_Jump,d0
-        //    jsr	(PlaySound).l	; play jumping sound
-        //    move.b	#$13,y_radius(a0)
-        //    move.b	#9,x_radius(a0)
-        //    btst	#2,status(a0)
-        //    bne.s	Sonic_RollJump
-        //    move.b	#$E,y_radius(a0)
-        //    move.b	#7,x_radius(a0)
-        //    move.b	#AniIDSonAni_Roll,anim(a0)	; use "jumping" animation
-        //    bset	#2,status(a0)
-        //    addq.w	#5,y_pos(a0)
-
-        //return_1AAE6:
-        //    rts
-        //; ---------------------------------------------------------------------------
-        //; loc_1AAE8:
-        //Sonic_RollJump:
-        //    bset	#4,status(a0)	; set the rolling+jumping flag
-        //    rts
-        //; End of function Sonic_Jump
-        
-
-        //; ---------------------------------------------------------------------------
-        //; Subroutine letting Sonic control the height of the jump
-        //; when the jump button is released
-        //; ---------------------------------------------------------------------------
-
-        //; ||||||||||||||| S U B R O U T I N E |||||||||||||||||||||||||||||||||||||||
-
-        //        ; ===========================================================================
-        //; loc_1AAF0:
-        //Sonic_JumpHeight:
-        //    tst.b	jumping(a0)	; is Sonic jumping?
-        //    beq.s	Sonic_UpVelCap	; if not, branch
-
-        //    move.w	#-$400,d1
-        //    btst	#6,status(a0)	; is Sonic underwater?
-        //    beq.s	+		; if not, branch
-        //    move.w	#-$200,d1
-        //+
-        //    cmp.w	y_vel(a0),d1	; is Sonic going up faster than d1?
-        //    ble.s	+		; if not, branch
-        //    move.b	(Ctrl_1_Held_Logical).w,d0
-        //    andi.b	#button_B_mask|button_C_mask|button_A_mask,d0 ; is a jump button pressed?
-        //    bne.s	+		; if yes, branch
-        //    move.w	d1,y_vel(a0)	; immediately reduce Sonic's upward speed to d1
-        //+
-        //    tst.b	y_vel(a0)		; is Sonic exactly at the height of his jump?
-        //    beq.s	Sonic_CheckGoSuper	; if yes, test for turning into Super Sonic
-        //    rts
-        //; ---------------------------------------------------------------------------
-        //; loc_1AB22:
-        //Sonic_UpVelCap:
-        //    tst.b	spindash_flag(a0)	; is Sonic charging a spindash or in a rolling-only area?
-        //    bne.s	return_1AB36		; if yes, return
-        //    cmpi.w	#-$FC0,y_vel(a0)	; is Sonic moving up really fast?
-        //    bge.s	return_1AB36		; if not, return
-        //    move.w	#-$FC0,y_vel(a0)	; cap upward speed
-
-        //return_1AB36:
-        //    rts
-        //; End of subroutine Sonic_JumpHeight
-        #endregion
-
+  
         if (_jump)
         {
             _jumping = true;
@@ -284,6 +179,9 @@ public class PlayerMovement : MonoBehaviour
 	void DMovement()
 	{
 		if (_moveLeft) {
+
+            // Turn Sprite Left
+            YRotation = FACING_LEFT;
 			
 			if (CurrentSpeed > 0f) {
 				CurrentSpeed = CurrentSpeed - DECELERATION;
@@ -296,10 +194,10 @@ public class PlayerMovement : MonoBehaviour
 				}
 			}
 			
-			// Turn Sprite Left
-			YRotation = FACING_LEFT;
-			
 		} else if (_moveRight) {
+
+            // Turn Sprite Right
+            YRotation = FACING_RIGHT;
 			
 			if (CurrentSpeed < 0f) {
                 CurrentSpeed = CurrentSpeed + DECELERATION;
@@ -311,9 +209,6 @@ public class PlayerMovement : MonoBehaviour
 					CurrentSpeed = TOP_SPEED;
 				}
 			}
-			
-			// Turn Sprite Right
-			YRotation = FACING_RIGHT;
 			
 		} else {
 			CurrentSpeed = CurrentSpeed - (Mathf.Min(Mathf.Abs(CurrentSpeed), FRICTION)*Mathf.Sign(CurrentSpeed));
@@ -331,8 +226,8 @@ public class PlayerMovement : MonoBehaviour
 		anim.SetFloat ("Speed", Mathf.Abs(CurrentSpeed));
 		anim.SetBool ("Jumping", _jumping);
         anim.SetFloat("EdgeDistance", _edgeDistance);
-        anim.SetBool("FrontEdge", _edgeFront);
-        anim.SetBool("BackEdge", _edgeBack);
+        anim.SetBool("EdgeFound", _edgeFound);
+        anim.SetFloat("Direction", YRotation);
 	}
 
     void ApplyGravity()
@@ -349,7 +244,15 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-	void GroundState()
+    void GroundState()
+    {
+        if (grounded)
+        {
+
+        }
+    }
+
+	void Collision()
 	{
 		Vector3 vGaStart, vGbStart;
 
@@ -360,32 +263,21 @@ public class PlayerMovement : MonoBehaviour
 			switch(_jumping)
 			{
 			case true:
-				if (YRotation == FACING_RIGHT)
-				{
 					vGaStart = new Vector3(box.center.x + 7,box.center.y,transform.position.z);
 					vGbStart = new Vector3(box.center.x - 7,box.center.y,transform.position.z);
-				}else
-				{
-					vGaStart = new Vector3(box.center.x - 7,box.center.y,transform.position.z);
-					vGbStart = new Vector3(box.center.x + 7,box.center.y,transform.position.z);
-				}
 				break;
 			default:
-				if (YRotation == FACING_RIGHT)
-				{
 					vGaStart = new Vector3(box.center.x + 9,box.center.y,transform.position.z);
 					vGbStart = new Vector3(box.center.x - 9,box.center.y,transform.position.z);
-				}else
-				{
-					vGaStart = new Vector3(box.center.x - 9,box.center.y,transform.position.z);
-					vGbStart = new Vector3(box.center.x + 9,box.center.y,transform.position.z);
-				}
 				break;
 			}
 
-			RaycastHit hGa, hGb;
+            RaycastHit hGa, hGb;
 
-            float distance = (box.height / 2)+(grounded? margin: Mathf.Abs (velocity.y));
+            // TODO: Fix this
+            // This may need to be revised - This is what was causing the bouncing issue, but dividing the velocity has helped
+            float distance = (box.height / 2) +(grounded? margin: Mathf.Abs (velocity.y/1.5f));
+
 
 			// No were not connected, no yet anyway
 			bool bGaConnected = false;
@@ -414,41 +306,39 @@ public class PlayerMovement : MonoBehaviour
 				if (hGa.collider != null) {SensorGroundA = hGa.point;}
 				if (hGb.collider != null) {SensorGroundB = hGb.point;}
 
-				// Fixes a weird bug where a and b although the same height seem to give different distances
-				if (hGa.distance > hGb.distance)
-				{
-					transform.Translate(Vector3.down * (hGa.distance - box.height/2)); // Places the transform on the ground
-				}
-				else if (hGa.distance < hGb.distance)
-				{
-					transform.Translate(Vector3.down * (hGb.distance - box.height/2)); // Places the transform on the ground
-				}
-                velocity = new Vector2(velocity.x, 0);
-			}
+                // Fixes a weird bug where a and b although the same height seem to give different distances
+                if (_hgaDistance > _hgbDistance)
+                {
+                    transform.Translate(Vector3.down * (_hgaDistance - box.height / 2)); // Places the transform on the ground
 
-			// Uh Oh not on the ground here, were going to fall :O
-			if (!bGaConnected && !bGbConnected){
-				grounded = false;
-				_edgeFront = false;
+                }
+                else if (_hgaDistance < _hgbDistance)
+                {
+                    transform.Translate(Vector3.down * (_hgbDistance - box.height / 2)); // Places the transform on the ground
+                }
+                velocity = new Vector2(velocity.x, 0);
+
+
 			}
+            else
+            {
+                grounded = false;
+            }
 
 			// Edge detection for the Balancing Animations
 			if (!bGaConnected && bGbConnected && !_jumping)
 			{
-				_edgeFront = true;
-                _edgeBack = false;
+				_edgeFound = true;
 				_edgeDistance = Mathf.Abs(((SensorGroundA.x - hGb.point.x) / 2));
 			}
             else if (bGaConnected && !bGbConnected && !_jumping)
             {
-                _edgeFront = false;
-                _edgeBack = true;
-                _edgeDistance = Mathf.Abs(((SensorGroundA.x - hGb.point.x) / 2));
+                _edgeFound = true;
+                _edgeDistance = Mathf.Abs(((SensorGroundB.x - hGa.point.x) / 2));
             }
             else
             {
-                _edgeBack = false;
-                _edgeFront = false;
+                _edgeFound = false;
             }
 		}
 	}
